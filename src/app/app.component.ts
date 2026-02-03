ce9cc1e9a3850928677dfe4b55f8e51e87be8481
Jarrad L. <jlovelace2500k@gmail.com>
2026-02-03 09:40:37 -0800

    Fixed and created header component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from ./components/header/Header.component';
+import { header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  template: `
-    <div class="flex flex-col items-center justify-center min-h-screen bg-slate-50 text-slate-900 font-sans">
-      
-      <h1 class="text-4xl font-bold mb-4">{{ title() }}</h1>
-      
-      <p class="mb-6">
-        Status: 
-        <span class="bg-[--color-brand] text-white px-3 py-1 rounded shadow-sm text-sm">
-          Zoneless v21.1
-        </span>
-      </p>
-      
-      <button 
-        (click)="updateTitle()" 
-        class="px-6 py-2 bg-slate-800 text-white rounded-lg hover:bg-slate-700 transition-all active:scale-95 shadow-md">
-        Refresh Title
-      </button>
-      
-      <div class="mt-8 w-full max-w-4xl">
-        <router-outlet />
-      </div>
-    </div>
-  `
+  imports: [RouterOutlet, header],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = signal('Jarrad\'s Portfolio');
 
-  updateTitle() {
-    this.title.set('Work in Progress...');
-  }
+export class AppComponent {
+  title = signal("Jarrad's Portfolio");
 }
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+// check out Calendly You share a link, and people can book a 15-minute slot on your calendar. You get their info first, and no one gets your number until you're ready.
+//check out google voice that forwards my phone and can change my number to avoid telemarketers
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
+export class header {
+  appName = signal('Jarrad\'s Portfolio Site');
+  name = signal('Jarrad Lovelace');
+  email = signal('jlovelace2500k@gmail.com');
+  linkedin = signal('https://www.linkedin.com/in/jarrad-lovelace-b07085115/ ');
+  position = signal('Full Stack Engineer');
 }

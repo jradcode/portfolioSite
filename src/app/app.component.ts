1bcb5e43bd6c4b898e2e0ee29b80f94928fe369b
Jarrad L. <jlovelace2500k@gmail.com>
2026-02-04 09:31:03 -0800

    Added a working navbar component. Added the pages to navbar. Next do some redesign of the header and make resume download work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { header } from './components/header/header';
+import { navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, header],
+  imports: [RouterOutlet, header, navbar],
   templateUrl: './app.component.html'
 })
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { home } from './pages/home/home';
+import { about } from './pages/about/about';
+import { resume } from './pages/resume/resume';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: home },
+  { path: 'about', component: about },
+  { path: 'resume', component: resume },
+  { path: '**', redirectTo: 'home' } // Wildcard route for 404s
+];

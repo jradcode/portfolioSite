bf4ef0d8ab90ed57b332d43ef83eae41bf9e914e
Jarrad L. <jlovelace2500k@gmail.com>
2026-03-10 20:45:18 -0700

    Made changes to appconfig with hash routing to prevent github pages from freezing and going blank when it is refreshed.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions, withHashLocation } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from '../interceptors/auth-interceptor';
 import { loadingInterceptor } from '../interceptors/loading-interceptor';
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(), 
     
     // Standard Routing
-    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideRouter(routes, withHashLocation(), withComponentInputBinding(), withViewTransitions()),
 
     // Modern HTTP client (optimized for 2026 browsers)
     provideHttpClient(withFetch(), withInterceptors([authInterceptor, loadingInterceptor, errorInterceptor])),
